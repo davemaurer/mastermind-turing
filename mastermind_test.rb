4544484d7ddf0586e8920fd41ce5657dc33959bf
David Maurer <davemaurermail@gmail.com>
2016-10-10 01:00:58 -0600

    Make overly complex test for creating a random winning combination each time. Passing.

--- mastermind_test.rb
+++ mastermind_test.rb
@@ -11,9 +11,17 @@ describe Mastermind do
     assert Mastermind
   end
 
-  it 'starts with a winning combination' do
+  it 'starts with a secret winning combination' do
     assert @mm.secret
     assert_equal 4, @mm.secret.length
+  end
+
+  it 'creates a random winning combination each time' do
+    base = Mastermind.new
+    mm1, mm2, mm3, mm4, mm5, mm6, mm7, mm8, mm9, mm10 = Mastermind.new
+    minds = [mm1, mm2, mm3, mm4, mm5, mm6, mm7, mm8, mm9, mm10]
+    same = minds.map { |m| return false unless m.secret == base.secret }
+    refute same
   end
 
   it 'wins' do
