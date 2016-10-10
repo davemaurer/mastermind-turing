7841d7889216d66a7da118e7e4ea4a2a902e7c52
David Maurer <davemaurermail@gmail.com>
2016-10-10 02:13:02 -0600

    Add test for reading a player response when prompted.

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -15,4 +15,15 @@ class Mastermind
     4.times { secret << key[rand(0..3)] }
     secret
   end
+
+  def greet_player
+    puts "\n Hello! Let's play Mastermind!"
+  end
+
+  def read_player_input
+    gets.chomp
+  end
 end
+
+mm = Mastermind.new
+mm.greet_player
--- mastermind_test.rb
+++ mastermind_test.rb
@@ -26,7 +26,24 @@ describe Mastermind do
 
   it 'wins' do
     skip
-    result = @mm.execute("BBGB")
-    assert result.message.downcase.include?("win")
+    result = @mm.execute('BBGB')
+    assert result.message.downcase.include?('win')
+  end
+
+  it 'captures a player response' do
+    with_stdin do |player|
+      player.puts 'y'
+      assert_equal 'y', @mm.read_player_input
+    end
+  end
+
+  private
+  def with_stdin
+    stdin = $stdin
+    $stdin, write = IO.pipe
+    yield write
+  ensure
+    write.close
+    $stdin = stdin
   end
 end
