3dbc7727cc0d55926deb56f1475bba18b9fb5045
David Maurer <davemaurermail@gmail.com>
2016-10-11 23:50:11 -0600

    Add tests for start and stop time.

--- test/timer_test.rb
+++ test/timer_test.rb
@@ -1,13 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/timer'
+require './lib/timer'
 
 describe Timer do
   before do
     @timer = Timer.new
   end
 
-  
+  it 'has a start time' do
+    assert @timer.start
+  end
+
+  it 'has a stop time' do
+    assert @timer.stop
+  end
 end
 
 
