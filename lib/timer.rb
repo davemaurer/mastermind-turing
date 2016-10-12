6bdfce2c19ffacff5eede54f3a1ff2b54535b36a
David Maurer <davemaurermail@gmail.com>
2016-10-11 23:16:48 -0600

    define Timer class so I can keep track of how long a player takes.

--- lib/timer.rb
+++ lib/timer.rb
@@ -1,3 +1,18 @@
 class Timer
-  
+  def initialize
+    @start_time = nil
+    @stop_time  = nil
+  end
+
+  def start
+    @start_time = Time.now
+  end
+
+  def stop
+    @end_time = Time.now
+  end
+
+  def time_taken
+    @end_time - @start_time
+  end
 end
