01777900ab6f4fd046564f3ffc782a4f2d987eab
David Maurer <davemaurermail@gmail.com>
2016-11-17 23:15:57 -0700

    Make message output look better in console. Looks worse in editor now though.

--- lib/messages.rb
+++ lib/messages.rb
@@ -16,21 +16,21 @@ module Messages
   end
 
   def print_instructions
-    puts "Once you hit (p)lay, you will guess four colors from (R)ed, (B)lue, (G)reen (Y)ellow.
-          an example guess looks like this: gryg . After you guess, I will tell you how many colors
-          you got right, and how many positions you got right. Got it? Ok, let's play."
+    puts "  Once you hit (p)lay, you will guess four colors from (R)ed, (B)lue, (G)reen (Y)ellow.
+  an example guess looks like this: gryg . After you guess, I will tell you how many colors
+  you got right, and how many positions you got right. Got it? Ok, let's play."
   end
 
   def print_answer(secret)
-    puts "The answer is #{secret}. And. You CHEATED!!!"
+    puts "  The answer is #{secret}. And. You CHEATED!!!"
   end
 
   def ask_to_play_again
-    puts 'Would you like to (p)lay again? If not you can (q)uit, or get the (i)nstructions.'
+    puts '  Would you like to (p)lay again? If not you can (q)uit, or get the (i)nstructions.'
   end
 
   def announce_invalid_input(game_started)
-    err = "I can 't understand that. "
+    err = "I can't understand that. "
     if game_started
       puts err + 'Please enter a guess (example: bgry) or you can (q)uit.'
     else
@@ -39,13 +39,13 @@ module Messages
   end
 
   def give_guess_feedback(color_count, position_count)
-    puts "You guessed #{color_count} correct colors, and #{position_count} correct positions.
-          Please enter another guess."
+    puts "  You guessed #{color_count} correct colors, and #{position_count} correct positions.
+  Please enter another guess."
   end
 
   def print_player_wins(guess, time, guesses)
-    puts "Congratulations! You guessed the secret answer, which was #{guess}!
-          It took you #{time} seconds and #{guesses} guesses. Nice job!!!"
+    puts "  Congratulations! You guessed the secret answer, which was #{guess}!
+  It took you #{time} seconds and #{guesses} guesses. Nice job!!!"
   end
 
   def print_cant_cheat_yet
